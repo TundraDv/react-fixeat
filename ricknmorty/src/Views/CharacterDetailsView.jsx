de8fd4d35d718fb7f8cf5f975896eb1b8a07414e
tundraElie <tundra.elie@gmail.com>
2024-08-04 07:24:04 -0400

    centered

--- ricknmorty/src/Views/CharacterDetailsView.jsx
+++ ricknmorty/src/Views/CharacterDetailsView.jsx
@@ -16,42 +16,44 @@ function CharacterDetailsView() {
     )
   }
   return (
-    <Container sx={{ padding: 5, margin: 4 }}>
-      <Typography variant="h2">{character.name}</Typography>
-      <Card sx={{ padding: 5, margin: 1 }}>
-        <Stack 
-          direction={{ xs: 'column', sm: 'row' }}
-          spacing={3}
-          alignItems="center"
-        >
-          <img 
-            src={character.image} 
-            alt={character.name} 
-            sx={{ maxWidth: '100%', height: 'auto' }} 
-          />
-          <Stack>
-            <Stack direction="row" spacing={3} alignItems="center">
-              <Stack spacing={3}>
-                <Typography><b>Status:</b></Typography>
-                <Typography><b>Species:</b></Typography>
-                <Typography><b>Gender:</b></Typography>
-                <Typography><b>Origin:</b></Typography>
-                <Typography><b>Location:</b></Typography>
-                <Typography><b>Created:</b></Typography>
-              </Stack>
-              <Stack spacing={3}>
-                <Typography>{character.status}</Typography>
-                <Typography>{character.species}</Typography>
-                <Typography>{character.gender}</Typography>
-                <Typography>{character.origin.name}</Typography>
-                <Typography>{character.location.name}</Typography>
-                <Typography>{new Date(character.created).toLocaleString()}</Typography>
+    <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+      <Container sx={{ padding: 5, margin: 4 }}>
+        <Typography variant="h2"><b>{character.name}</b></Typography>
+        <Card sx={{ padding: 5, margin: 1 }}>
+          <Stack 
+            direction={{ xs: 'column', sm: 'row' }}
+            spacing={3}
+            alignItems="center"
+          >
+            <img 
+              src={character.image} 
+              alt={character.name} 
+              sx={{ maxWidth: '100%', height: 'auto' }} 
+              />
+            <Stack>
+              <Stack direction="row" spacing={3} alignItems="center">
+                <Stack spacing={3}>
+                  <Typography><b>Status:</b></Typography>
+                  <Typography><b>Species:</b></Typography>
+                  <Typography><b>Gender:</b></Typography>
+                  <Typography><b>Origin:</b></Typography>
+                  <Typography><b>Location:</b></Typography>
+                  <Typography><b>Created:</b></Typography>
+                </Stack>
+                <Stack spacing={3}>
+                  <Typography>{character.status}</Typography>
+                  <Typography>{character.species}</Typography>
+                  <Typography>{character.gender}</Typography>
+                  <Typography>{character.origin.name}</Typography>
+                  <Typography>{character.location.name}</Typography>
+                  <Typography>{new Date(character.created).toLocaleString()}</Typography>
+                </Stack>
               </Stack>
             </Stack>
           </Stack>
-        </Stack>
-      </Card>
-    </Container>
+        </Card>
+      </Container>
+    </Box>
   );
 }
 
